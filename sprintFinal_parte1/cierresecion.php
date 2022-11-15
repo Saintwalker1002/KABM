7a5cad1238ef655315bd20c53028ce71b14160cc
Martin Vallejo <mvallejob32@gmail.com>
2022-11-15 01:28:46 -0300

    login final

---
+++ sprintFinal_parte1/cierresecion.php
@@ -0,0 +1,8 @@
+<?php
+
+    include_once('registar.php');
+
+    session_destroy();
+    header("location:");
+
+?>
---
+++ sprintFinal_parte1/main.js
@@ -0,0 +1,24 @@
+var xmlhttp = new XMLHttpRequest();
+
+function guardar(){
+       
+    var nombres = document.getElementById('nombres').value;
+    var apellidos = document.getElementById('apellidos').value;
+    var usuario = document.getElementById('usuario').value;
+    var correo = document.getElementById('correo').value;
+    var password = document.getElementById('password').value;
+    var password_c = document.getElementById('password_c').value;
+       
+   
+    xmlhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status ==200){
+            mostrar_datos();
+            document.getElementById('formulario').reset()
+        }
+    }
+    xmlhttp.open("POST","insertar.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify({nombres:nombres, apellidos:apellidos, usuario:usuario, correo:correo, password:password, password_c:password_c}));
+       
+   
+}
